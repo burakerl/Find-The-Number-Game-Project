ee41b230536a14777329f3062ba37389914cad81
Burak EROL <burakerol.j@gmail.com>
2026-01-06 07:07:53 +0300

    created

---
+++ script.js
@@ -0,0 +1,82 @@
+// VARIABLES
+let randomNumber = Math.trunc(Math.random() * 100);
+let hp = 6;
+console.log(randomNumber);
+
+//DOM ELEMENTS
+const userNumber = document.getElementById("number");
+const submitButton = document.getElementById("btn");
+const hint = document.querySelector(".hint");
+const hintContainer = document.querySelector(".hint-container");
+const againButton = document.createElement("button");
+againButton.classList.add("again-btn");
+const heartContainer = document.querySelector(".heart-container");
+const heart = document.querySelector(".fa-solid");
+const imgContainer = document.querySelector(".img-container");
+const imgNumber = document.createElement("h1");
+imgNumber.classList.add("img-number");
+
+// BUTTON CLICKS
+submitButton.addEventListener("click", evaluateInput);
+againButton.addEventListener("click", playAgain);
+
+// FUNCTIONS
+//Player wins
+function winState() {
+  hint.innerHTML = `Good Job!`;
+  hintContainer.style.backgroundColor = "cyan";
+  againButton.innerHTML = "Again";
+  hintContainer.appendChild(againButton);
+  submitButton.disabled = true;
+}
+
+//Get and Evaluate Input
+function evaluateInput() {
+  const number = userNumber.value;
+
+  if (number === "") {
+    alert("Lütfen Bir Sayı Girin.");
+  } else if (isNaN(number)) {
+    alert("Lütfen Bir Sayı Girin.");
+  } else if (number > randomNumber) {
+    hint.innerHTML = `Guess lower!`;
+    loseHp();
+    console.log(hp);
+  } else if (number < randomNumber) {
+    hint.innerHTML = `Guess higher!`;
+    loseHp();
+    console.log(hp);
+  } else if (number == randomNumber) {
+    winState();
+  }
+
+  imgNumber.innerHTML = `${userNumber.value}`;
+  imgContainer.appendChild(imgNumber);
+
+  userNumber.value = "";
+}
+
+//Player loses
+function loseState() {
+  hint.innerHTML = `You lose! It was ${randomNumber}`;
+  hintContainer.style.backgroundColor = "red";
+  againButton.innerHTML = "Again";
+  hintContainer.appendChild(againButton);
+  submitButton.disabled = true;
+  userNumber.disabled = true;
+}
+
+function loseHp() {
+  heartContainer.removeChild(heartContainer.firstElementChild);
+  hp -= 1;
+  checkHp();
+}
+function checkHp() {
+  if (hp == 0) {
+    loseState();
+  }
+}
+
+function playAgain() {
+  location.reload();
+}
